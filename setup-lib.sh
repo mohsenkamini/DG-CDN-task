d6e3d0dd93ece27344974870427ddf223830b105
Mohsen K Amini <77579794+mohsenkamini@users.noreply.github.com>
2022-08-08 19:17:30 +0430

    Create setup-lib.sh

---
+++ setup-lib.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+install_elk () {
+  
+  cd ./elk
+  docker-compose -f create-certs.yml run --rm create_certs
+  docker-compose -f secure-docker-compose.yml up -d
+  docker exec es01 /bin/bash -c "bin/elasticsearch-setup-passwords auto --batch --url https://es01:9200" > passwords
+  echo "Your passwords are stored in passwords file"
+  sed -i "s|KIBANA_ELASTICSEARCH_PASSWORD=.*|KIBANA_ELASTICSEARCH_PASSWORD=`grep "PASSWORD kibana_system" passwords| awk '{print $4}'`|g" .env
+  
+}
+
+
