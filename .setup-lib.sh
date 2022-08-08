5960d090a0cbfb905735292b36255e74381547ea
Mohsen K Amini <77579794+mohsenkamini@users.noreply.github.com>
2022-08-08 23:30:35 +0430

    Update .setup-lib.sh

--- .setup-lib.sh
+++ .setup-lib.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #set -euxo pipefail 
 
+install_docker_compose () {
 
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+	docker-compose --version
+}
 
 create_networks () {
 
