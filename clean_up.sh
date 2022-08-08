e6b8fb64adaf7738e5e99f411c4b07cb42d0ef0f
Mohsen K Amini <77579794+mohsenkamini@users.noreply.github.com>
2022-08-08 22:44:02 +0430

    Create clean_up.sh

---
+++ clean_up.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -euxo pipefail
+
+confirmate () {
+  case $1 in
+    -y)
+      ans=y
+      ;;
+     *)
+       read -r -p "This will delete all your docker containers,images and volumes. Are you SURE you want to do this?[y/n] " "ans"
+       ;;
+}
+
+
+clean_up () {
+
+  for i in `docker ps -q ` ; do docker stop $i ; docker rm $i ; done
+  docker system prune -f --volumes
+  for a in $(docker image ls | grep "$1" | awk {'print $3'}); do docker image rm -f $a; done
+  
+}
+
+confirmate $1
+
+case $ans in
+  y)
+    clean_up
+    ;;
+  n)
+    echo "you did not confirm for a clean up. exiting..."
+    exit 1
+    ;;
+  *)
+    echo "only y or n is acceptable"
+    exit 1
+    ;;
+
